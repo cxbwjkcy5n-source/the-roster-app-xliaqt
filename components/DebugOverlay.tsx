1e0e4cdf134ff1484cad63c1fd5019c27b9f13fd
natively.dev <api@natively.dev>
2026-03-15 02:25:13 +0000

    fix: Add missing Platform import to DebugOverlay

--- components/DebugOverlay.tsx
+++ components/DebugOverlay.tsx
@@ -8,6 +8,7 @@ import {
   Modal,
   ScrollView,
   Pressable,
+  Platform,
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Constants from 'expo-constants';
